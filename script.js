b7462e1ea7e04f6c1cb01a08022a15f61962d9dd
mdemon420 <mdeamon4@gmail.com>
2022-01-27 13:41:43 +0600

    add roll button and current poin add

--- script.js
+++ script.js
@@ -1,9 +1,32 @@
-const score0 = document.querySelector('#score--0');
-const score1 = document.querySelector('#score--1');
-const dice = document.querySelector('.dice');
+// Elements
+const score0El = document.querySelector('#score--0');
+const score1El = document.querySelector('#score--1');
+const diceEl = document.querySelector('.dice');
+const btnNew = document.querySelector('.btn--new');
+const btnRoll = document.querySelector('.btn--roll');
+const btnHold = document.querySelector('.btn--hold');
+const current0El = document.querySelector('#current--0');
+const current1El = document.querySelector('#current--1');
 
-score0.textContent = 0;
-score1.textContent = 0;
-dice.classList.add('hidden');
+// condition
+score0El.textContent = 0;
+score1El.textContent = 0;
+diceEl.classList.add('hidden');
+
+let current = 0;
+
+btnRoll.addEventListener('click', function(){
+    const dice = Math.trunc(Math.random()*6)+1;
+    console.log(dice);
+    
+    diceEl.classList.remove('hidden');
+    diceEl.src = `dice-${dice}.png`;
+
+    if(dice !== 1){
+        current += dice;
+        current0El.textContent = current;
+    }
+
+});
 
 
